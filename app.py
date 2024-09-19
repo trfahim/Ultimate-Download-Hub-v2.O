043e2b1ad165742fe67eb1f0a26e2e101296c89e
Tahsan Rahman Fahim <80458361+trfahim@users.noreply.github.com>
2024-09-19 19:40:58 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,182 @@
+from flask import Flask, render_template, request
+import requests
+import time,emoji
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/pc-games', methods=['GET', 'POST'])
+def pc_games():
+    game_found = None
+    download_link_1 = None
+    download_link_2 = None
+    download_link_3 = None
+
+    if request.method == 'POST':
+        game_name = request.form['game_name']
+        
+        search_url_1 = f"https://www.apunkagames.com/{(game_name.replace(' ', '-')).lower()}/"
+        response = requests.get(f'{search_url_1}').status_code
+        
+        search_url_2 = f"https://oceanofgames.com/{(game_name.replace(' ', '-')).lower()}/"
+        response_2 = requests.get(f'{search_url_2}').status_code 
+        
+        search_url_3 = f"https://dodi-repacks.site/{(game_name.replace(' ', '-')).lower()}/"
+        response_3 = requests.get(f'{search_url_3}').status_code
+        
+        
+        if response == 404:
+            game=0
+        else:
+            download_link_1 = search_url_1
+            game_found = True
+        
+        if response_2 == 404:
+            game=0
+        else:
+            download_link_2 = search_url_2
+            game_found = True
+        
+        if response_3 == 404:
+            game=0
+        else:
+            download_link_3 = search_url_3
+            game_found = True
+            
+        if response==404 and response_2==404 and response_3==404:
+            game_found = False
+        else:
+            game=0
+    
+    return render_template('pc_games.html',
+                           game_found=game_found, 
+                           download_link_1=download_link_1, 
+                           download_link_2=download_link_2,
+                           download_link_3=download_link_3,
+                           )
+    
+
+@app.route('/android-games', methods=['GET', 'POST'])
+def android_games():
+    game_found = None
+    download_link_1 = None
+    download_link_2 = None
+    download_link_3 = None
+    
+    if request.method == 'POST':
+        game_name = request.form['game_name']
+        
+        search_url_1 = f"https://apkdone.com/{(game_name.replace(' ', '-')).lower()}/"
+        response_1 = requests.get(f'{search_url_1}').status_code
+        
+        search_url_2 = f"https://apkpure.com/search?q={(game_name.replace(' ', '-')).lower()}"
+        response_2 = requests.get(f'{search_url_2}').status_code
+        
+        search_url_3 = f"https://androeed.store/files/{(game_name.replace(' ', '-')).lower()}.html"
+        response_3 = requests.get(f"{search_url_3}").status_code
+        
+        time.sleep(3)
+        
+        if response_1 == 404:
+            game=0
+        else:
+            download_link_1=search_url_1
+            game_found = True
+        
+        if response_2 == 404:
+            game=0
+        else:
+            download_link_2=search_url_2
+            game_found=True
+        
+        if response_3 == 404:
+            game=0
+        else:
+            download_link_3=search_url_3
+            game_found=True
+        
+        if response_1==404 and response_2==404 and response_3==404:
+            game_found=False
+        else:
+            game=0
+        
+    return render_template('android_games.html',
+                           game_found=game_found,
+                           download_link_1=download_link_1,
+                           download_link_2=download_link_2,
+                           download_link_3=download_link_3)
+    
+    
+
+@app.route('/movies', methods=['GET', 'POST'])
+def movies():
+    movie_found = None
+    download_link_1 = None
+    download_link_2 = None
+    download_link_3 = None
+    download_link_4 = None
+    
+    if request.method == 'POST':
+        movie_name = request.form['movie-name']
+        
+        search_url_1 = f"https://cinedoze.com/{(movie_name.replace(' ','-')).lower()}/"
+        response_1 = requests.get(f'{search_url_1}').status_code
+        
+        search_url_2 = f"https://mlsbd.shop/{(movie_name.replace(' ', '-')).lower()}"
+        response_2 = requests.get(f"{search_url_2}").status_code
+         
+        search_url_3 = f"https://hdhub4u.contact/{(movie_name.replace(' ', '-')).lower()}/"
+        response_3 = requests.get(f"{search_url_3}").status_code
+        
+        search_url_4 = f"https://mlwbd.lv/{(movie_name.replace(' ', '-')).lower()}/"
+        response_4 = requests.get(f"{search_url_4}").status_code
+        
+        
+        if response_1 == 404:
+            game=0
+        else:
+            download_link_1=search_url_1
+            movie_found=True
+        
+        if response_2 == 404:
+            game=0
+        else:
+            download_link_2=search_url_2
+            movie_found=True
+        
+        if response_3 == 404:
+            game=0
+        else:
+            download_link_3=search_url_3
+            movie_found=True
+        
+        if response_4 == 404:
+            game=0
+        else:
+            download_link_4=search_url_4
+            movie_found=True
+        
+           
+        if response_1==404 and response_2==404 and response_3==404 and response_4==404:
+            movie_found=False
+            
+    return render_template('movies.html',
+                           movie_found=movie_found,
+                           download_link_1=download_link_1,
+                           download_link_2=download_link_2,
+                           download_link_3=download_link_3,
+                           download_link_4=download_link_4)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/exit')
+def exit():
+    return "Thanks for visiting!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
